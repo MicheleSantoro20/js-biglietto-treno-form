12ef16656bc07e96ef663206d5f5a9ff5e761749
MicheleSantoro20 <michelesantoro2000@icloud.com>
2023-02-23 15:00:38 +0100

    js smette di funzionare e non riporta il nome nell'id "nomePasseggero"

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 const domSubmitButton = document.getElementById ('#submit');
 const domResetButton = document.getElementById ('#reset');
 
-const nomeCliente = document.getElementById('#nomeCliente')
+const nomeCliente = document.getElementById('#nomeCliente');
 const nameReported = nomeCliente.value ;
 
 
 domSubmitButton.addEventListener('click', 
 
      function ()  {
-        const nomePasseggero = document.getElementById ('#nomePasseggero')
+        const nomePasseggero = document.getElementById ('#nomePasseggero');
+        nomePasseggero.innerHTML = `${nameReported}`;
     });
