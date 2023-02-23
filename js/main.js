ff34759bb50490e5c374183ce78f49e92d2e0c67
MicheleSantoro20 <michelesantoro2000@icloud.com>
2023-02-23 14:59:19 +0100

    Foglio lavorativo creato, html impostato

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const domSubmitButton = document.getElementById ('#submit');
+const domResetButton = document.getElementById ('#reset');
+
+const nomeCliente = document.getElementById('#nomeCliente')
+const nameReported = nomeCliente.value ;
+
+
+domSubmitButton.addEventListener('click', 
+
+     function ()  {
+        const nomePasseggero = document.getElementById ('#nomePasseggero')
+    });
