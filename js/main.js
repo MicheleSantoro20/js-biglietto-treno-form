bfe87a60b9b1cd3bc4280faa1ca3d3dc629d3350
MicheleSantoro20 <michelesantoro2000@icloud.com>
2023-02-23 17:14:47 +0100

    Js checkpoint on priceKm

--- js/main.js
+++ js/main.js
@@ -2,12 +2,35 @@ const domSubmitButton = document.getElementById ('submit');
 const domResetButton = document.getElementById ('reset');
 
 const nomeCliente = document.querySelector('#nomeCliente');
-const nameReported = nomeCliente.value ;
+let km = document.querySelector('#nKm');
+let offerte = document.querySelector('#offerte');
+const minorenne = document.querySelector('#value1');
+kmPercorsi = parseInt(km.value);
+priceKm = kmPercorsi * 0.21;
+
+console.log(priceKm);
+
 
 
 domSubmitButton.addEventListener('click', 
 
      function ()  {
-        const nomePasseggero = document.getElementById ('nomePasseggero');
-        nomePasseggero.innerHTML = `${nameReported}`;
+
+        nomePasseggero.innerHTML = `${nomeCliente.value}`;
+        let età = document.querySelector('#age').value;
+
+        if (età == "minorenne"){
+            offerte.innerHTML = 'Offerta Minorenni';
+            
+        }else if  (età == "maggiorenne"){
+            offerte.innerHTML = 'Nessuna offerta';
+        } else {
+            offerte.innerHTML = 'Offerta Silver';
+        }
+
+        console.log(priceKm);
+
+        
+
+
     });
